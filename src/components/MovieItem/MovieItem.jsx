fefb80c38ea4a2fa561521b259961033ab28fc14
Johnshandsable <Jshands@asu.edu>
2021-03-07 10:41:13 -0600

    added styling to movie item

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -3,12 +3,9 @@ import { useDispatch } from 'react-redux';
 
 import DescriptionIcon from '@material-ui/icons/Description';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
+import Paper from '@material-ui/core/Paper';
 import { red } from '@material-ui/core/colors';
 import Typography from '@material-ui/core/Typography';
 import { Description } from '@material-ui/icons';
@@ -20,13 +17,13 @@ function MovieItem({ movie }) {
   /* Styles */
   const useStyles = makeStyles((theme) => ({
     root: {
-      maxWidth: 200,
-      height: 300,
-    },
-    media: {
-      // height: 0,
-      paddingTop: '150%',
-      // paddingTop: '56.25%', // 16:9
+      border: '3px solid black',
+      boxShadow: '0 0 10px',
+      marginBottom: 20,
+      marginTop: 20,
+      marginLeft: 50,
+      maxWidth: 185,
+      maxHeight: 400,
     },
     avatar: {
       backgroundColor: red[500],
@@ -49,21 +46,15 @@ function MovieItem({ movie }) {
   };
 
   return (
-    <Grid item xs={3}>
-      <Card className={classes.root}>
-        <CardHeader title={movie.title} />
-        <CardMedia
-          className={classes.media}
-          image={movie.poster}
-          title={movie.title}
+    <Grid item xs={4}>
+      <Paper elevation={3} className={classes.root}>
+        <Typography variant="body1">{movie.title}</Typography>
+        <img
+          onClick={() => handleImageClick({ movie })}
+          src={movie.poster}
+          alt={movie.title}
         />
-
-        {/* <img
-        onClick={() => handleImageClick({ movie })}
-        src={movie.poster}
-        alt={movie.title}
-      /> */}
-      </Card>
+      </Paper>
     </Grid>
   );
 }
