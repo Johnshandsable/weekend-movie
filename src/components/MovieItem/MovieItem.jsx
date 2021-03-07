aba0cbcddacc8d9592a109229df00fb7f9cc7ab6
Johnshandsable <Jshands@asu.edu>
2021-03-07 10:24:19 -0600

    adding more styling to movieitem

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,13 +1,45 @@
 import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
+import DescriptionIcon from '@material-ui/icons/Description';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import { red } from '@material-ui/core/colors';
 import Typography from '@material-ui/core/Typography';
+import { Description } from '@material-ui/icons';
 
 function MovieItem({ movie }) {
   const dispatch = useDispatch();
   const history = useHistory();
 
+  /* Styles */
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      maxWidth: 200,
+      height: 300,
+    },
+    media: {
+      // height: 0,
+      paddingTop: '150%',
+      // paddingTop: '56.25%', // 16:9
+    },
+    avatar: {
+      backgroundColor: red[500],
+    },
+  }));
+
+  const classes = useStyles();
+
+  /*
+    <IconButton aria-label="full description">
+          <DescriptionIcon />
+        </IconButton>
+  */
   const handleImageClick = ({ movie }) => {
     dispatch({
       type: 'FETCH_MOVIE',
@@ -15,14 +47,23 @@ function MovieItem({ movie }) {
     });
     history.push('/details');
   };
+
   return (
     <Grid item xs={3}>
-      <Typography variant="h6">{movie.title}</Typography>
-      <img
+      <Card className={classes.root}>
+        <CardHeader title={movie.title} />
+        <CardMedia
+          className={classes.media}
+          image={movie.poster}
+          title={movie.title}
+        />
+
+        {/* <img
         onClick={() => handleImageClick({ movie })}
         src={movie.poster}
         alt={movie.title}
-      />
+      /> */}
+      </Card>
     </Grid>
   );
 }
